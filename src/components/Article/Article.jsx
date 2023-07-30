fe11691028e091dc630e0d391c6b12c0609ab3dd
kooroosh1363 <peymanraad1363@gmail.com>
2023-07-30 12:00:07 +0400

    add content in each map for article

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,15 +1,15 @@
 import styled from "./Article.module.css";
 import article from "../../assets/image/article.gif";
 
-function Article(){
+function Article(props){
 
-
+    console.log(props.articleData.title);
     return(
         <div className={styled.articleWrapper}>
-            <img src={article}  />
+            <img src={props.articleData.imageUlr}  />
 
-            <h3>Lorem ipsum dolor sit amet...</h3>
-            <span>read article in 5 min</span>
+            <h3>{props.articleData.title}</h3>
+            <span>read article in {props.articleData.readingTime} min</span>
 
         </div>
     );
