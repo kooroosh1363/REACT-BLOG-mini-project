ea39b7d1d1f1dcd0045c2cdbb811e6c4653fabf6
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-03 09:22:16 +0400

    how create new post or article

--- src/pages/NewArticles/NewArticles.jsx
+++ src/pages/NewArticles/NewArticles.jsx
@@ -3,37 +3,51 @@ import Navbar from "../../components/navbar/Navbar";
 import styled from "./newArticles.module.css";
 import Input from "../../components/input/input";
 import TextArea from "../../components/textarea/TextArea";
+import Footer from "../../components/footer/Footer";
+import axios from "axios";
 function NewArticles() {
-    
+
     const [articles, setArticles] = useState({
         title: "",
         date: "",
-        readingTime:"",
-        author:"",
-        text:"",
+        readingTime: "",
+        author: "",
+        imageUrl: "",
+        text: "",
     });
 
     const onHandleArticle = (e) => {
-        setArticles((pervState) =>({
+        setArticles((pervState) => ({
             ...pervState,
-            [e.target.name]:e.target.value,
+            [e.target.name]: e.target.value,
         }));
     };
 
 
 
     const onHandleText = (e) => {
-        setArticles((pervState) =>({
+        setArticles((pervState) => ({
             ...pervState,
-            text:e.target.value,
+            text: e.target.value,
         }));
     };
 
 
+    const sendNewArticle = () => {
+        axios.post("http://localhost:8000/articles",
+            {
+                id:9 ,
+                imageUlr: articles.imageUrl,
+                title: articles.title,
+                readingTime: articles.readingTime,
+                date: articles.date,
+                Author: articles.author,
+                content: articles.text,
+            });
+    };
 
 
-
-    console.log(articles);
+    // console.log(articles);
 
     return (
         <>
@@ -41,14 +55,21 @@ function NewArticles() {
 
             <div className={styled.createArticlePage}>
                 <div className="container">
-                    <h1>create article :</h1>
-                    <Input label="Title:" name="title" placeHolder="name's article..." handleChange={onHandleArticle}/>
-                    <Input label="Date:" name="date" placeHolder="0000/00/00" handleChange={onHandleArticle}/>
-                    <Input label="ReadingTime:" name="readingTime" placeHolder="00 min" handleChange={onHandleArticle}/>
-                    <Input label="Author:" name="author" placeHolder="john doe" handleChange={onHandleArticle}/>
-                    <TextArea label="Text:" handleChange={onHandleText}/>
+                    <h1>Create Article :</h1>
+                    <Input label="Title:" name="title" placeHolder="name's article..." handleChange={onHandleArticle} type="text" />
+                    <Input label="Date:" name="date" placeHolder="0000/00/00" handleChange={onHandleArticle} type="text" />
+                    <Input label="ReadingTime:" name="readingTime" placeHolder="00 min" handleChange={onHandleArticle} type="text" />
+                    <Input label="Author:" name="author" placeHolder="john doe" handleChange={onHandleArticle} type="text" />
+                    <Input label="AddImage:" name="imageUrl" handleChange={onHandleArticle} type="text" />
+                    <TextArea label="Text:" handleChange={onHandleText} />
+
+                    <div className={styled.buttonWrapper}>
+                        <button onClick={sendNewArticle}>Send Article</button>
+                    </div>
                 </div>
             </div>
+
+            <Footer />
 
         </>
     );
